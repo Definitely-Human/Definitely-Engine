6de6a7831c0bfebe4d68889314b2dad7783e90a8
Definitely-Human <odest66@gmail.com>
2023-08-20 04:19:48 +0200

    Added premake.

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Defen"
+	architecture "x64"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Defen"
+	location "Defen"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DE_PLATFORM_WINDOWS",
+			"DE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "DE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Debug"
+		defines "DE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Defen/vendor/spdlog/include",
+		"Defen/src"
+	}
+
+	links
+	{
+		"Defen"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+			defines "DE_DEBUG"
+			symbols "On"
+
+	filter "configurations:Release"
+		defines "DE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Debug"
+		defines "DE_DIST"
+		optimize "On"
